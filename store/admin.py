b22113f0c1e7d8db6ae8e9becb97a9a76ee203d8
Vijay Thapa <vijaythapa333@gmail.com>
2021-05-22 22:43:07 +0545

    Category Added

--- store/admin.py
+++ store/admin.py
@@ -15,16 +15,16 @@ class CategoryAdmin(admin.ModelAdmin):
     list_filter = ('is_active', 'is_featured')
     list_per_page = 10
     search_fields = ('title', 'description')
-    prepopulated_fields = {"slug": ("title, ")}
+    prepopulated_fields = {"slug": ("title", )}
 
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'category', 'category_image', 'is_active', 'is_featured', 'updated_at')
+    list_display = ('title', 'slug', 'category', 'product_image', 'is_active', 'is_featured', 'updated_at')
     list_editable = ('slug', 'category', 'is_active', 'is_featured')
     list_filter = ('is_active', 'is_featured')
     list_per_page = 10
     search_fields = ('title', 'category', 'short_description')
-    prepopulated_fields = {"slug": ("title, ")}
+    prepopulated_fields = {"slug": ("title", )}
 
 
 admin.site.register(Address, AddressAdmin)
